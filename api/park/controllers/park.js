f0231e5a74d2ad954b6776517211ca613003719d
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-05-22 13:31:42 +0800

    add utils for react-admin

--- api/park/controllers/park.js
+++ api/park/controllers/park.js
@@ -1,8 +1,18 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils')
 
-/**
- * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  async find(ctx) {
+    let entities;
+    ctx.set('Content-Range', await strapi.services.park.count()) // <--- Add this guy
+    if (ctx.query._q) {
+      entities = await strapi.services.park.search(ctx.query)
+    } else {
+      entities = await strapi.services.park.find(ctx.query)
+    }
 
-module.exports = {};
+    return entities.map(entity =>
+      sanitizeEntity(entity, { model: strapi.models.park })
+    )
+  },
+}
